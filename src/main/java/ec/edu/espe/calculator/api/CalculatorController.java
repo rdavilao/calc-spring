507d268365fde49ca41f8aea688741d5a71806d9
rdavilao <rdavila@espe.edu.ec>
2021-03-19 17:29:18 -0500

    Add Swagger

--- src/main/java/ec/edu/espe/calculator/api/CalculatorController.java
+++ src/main/java/ec/edu/espe/calculator/api/CalculatorController.java
@@ -6,6 +6,9 @@
 package ec.edu.espe.calculator.api;
 
 import ec.edu.espe.calculator.service.CalculatorService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.math.BigDecimal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +32,11 @@ public class CalculatorController {
     }
 
     @GetMapping("/add")
+    @ApiOperation(value = "Suma de dos números", notes = "Se suma dos numeros y se devuelve su respuesta")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Numeros sumados"),
+        @ApiResponse(code = 400, message = "Error al sumar los numeros")
+    })
     public ResponseEntity add(@RequestParam BigDecimal number1, @RequestParam BigDecimal number2) {
         try {
             return ResponseEntity.ok(this.service.add(number1, number2));
@@ -38,6 +46,11 @@ public class CalculatorController {
     }
     
     @GetMapping("/substract")
+    @ApiOperation(value = "Resta de dos números", notes = "Se resta dos numeros y se devuelve su respuesta")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Numeros restados"),
+        @ApiResponse(code = 400, message = "Error al restar los numeros")
+    })
     public ResponseEntity substract(@RequestParam BigDecimal number1, @RequestParam BigDecimal number2) {
         try {
             return ResponseEntity.ok(this.service.substract(number1, number2));
@@ -47,6 +60,11 @@ public class CalculatorController {
     }
     
     @GetMapping("/multiply")
+    @ApiOperation(value = "Multiplicacion de dos números", notes = "Se multiplica dos numeros y se devuelve su respuesta")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Numeros multiplicados"),
+        @ApiResponse(code = 400, message = "Error al multiplicar los numeros")
+    })
     public ResponseEntity multiply(@RequestParam BigDecimal number1, @RequestParam BigDecimal number2) {
         try {
             return ResponseEntity.ok(this.service.multiply(number1, number2));
@@ -56,6 +74,11 @@ public class CalculatorController {
     }
     
     @GetMapping("/divide")
+    @ApiOperation(value = "Divide de dos números", notes = "Se divide dos numeros y se devuelve su respuesta")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Numeros divididos"),
+        @ApiResponse(code = 400, message = "Error al dividir los numeros")
+    })
     public ResponseEntity divide(@RequestParam BigDecimal number1, @RequestParam BigDecimal number2) {
         try {
             return ResponseEntity.ok(this.service.divide(number1, number2));
---
+++ src/main/java/ec/edu/espe/calculator/config/SpringFoxConfig.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.calculator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+@Import({SpringDataRestConfiguration.class})
+
+public class SpringFoxConfig {
+    
+     @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("ec.edu.espe.calculator.api"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+}
