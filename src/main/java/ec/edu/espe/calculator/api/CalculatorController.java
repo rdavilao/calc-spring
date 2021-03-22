e51489a92fe9878c185da1039aa44382e40b5cce
rdavilao <rdavila@espe.edu.ec>
2021-03-22 11:40:06 -0500

    Added SonarCloud Integration Updated: pom.xml, application.properties File Added: build.yml

--- src/main/java/ec/edu/espe/calculator/api/CalculatorController.java
+++ src/main/java/ec/edu/espe/calculator/api/CalculatorController.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.ApiResponses;
 import java.math.BigDecimal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
